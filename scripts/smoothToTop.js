0251fc670c51b0c7fa17986d444ed297ec5c41d0
lambrant <andreas.lambrant@live.se>
2015-10-09 10:49:57 +0000

    Fixed some minor problems with the back to top link

--- scripts/smoothToTop.js
+++ scripts/smoothToTop.js
@@ -6,11 +6,6 @@ $(document).ready(function() {
     var originalY = element.offset().top;
     var topMargin = 450;
     
-    element.css("position", "relative");
-    element.css("background-color", "black");
-    element.css("width", "50px");
-    element.css("margin-left", "4%");
-    
     $(window).on("scroll", function(event) {
         var scrollTop = $(window).scrollTop();
         
@@ -36,7 +31,12 @@ $(document).ready(function() {
         
      $("html, body").animate({ scrollTop: 0 }, "slow");
      return false;
-     
-  });
+    });
+  
+    $("a[href='#mobileTop']").click(function() {
+          
+       $("html, body").animate({ scrollTop: 0 }, "slow");
+       return false;
+    });
   
 });
