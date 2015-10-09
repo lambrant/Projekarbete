e07e91487377d183e127f4b2561736d85d2ecb59
lambrant <andreas.lambrant@live.se>
2015-10-09 10:08:36 +0000

    Done with the link to the top script

--- scripts/smoothToTop.js
+++ scripts/smoothToTop.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
     
+    //----------div that follows viewer when scrolling------
+    
+    var element = $("#goToTop");
+    var originalY = element.offset().top;
+    var topMargin = 450;
+    
+    element.css("position", "relative");
+    element.css("background-color", "black");
+    element.css("width", "50px");
+    element.css("margin-left", "4%");
+    
+    $(window).on("scroll", function(event) {
+        var scrollTop = $(window).scrollTop();
+        
+        element.stop(false, false).animate({
+            top: scrollTop < originalY
+                    ? 0
+                    : scrollTop - originalY + topMargin
+        }, 300);
+    });
+    
+    $(window).scroll(function() {   //to top link appears when scrolled down 100px
+        
+        if ($(this).scrollTop() > 100) {
+            $('#goToTop').fadeIn();
+        } else {
+            $('#goToTop').fadeOut();
+        }
+    });
+    
+    //----------click to get to top-------------------------
+    
     $("a[href='#top']").click(function() {
         
      $("html, body").animate({ scrollTop: 0 }, "slow");
