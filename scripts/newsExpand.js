b0097bfcab6b767deff9b169f47262dde147f508
lambrant <andreas.lambrant@live.se>
2015-10-09 09:13:37 +0000

    Done with the expandable news script, not a general solution though

--- scripts/newsExpand.js
+++ scripts/newsExpand.js
@@ -1,19 +1,85 @@
 $(document).ready(function() {
     
-   $(".expand").click(function() {
+    $("#news1").click(function() {
        
        var val = $(this).text();
        
        if (val === "More..") {
            
-           $(".newsParagraph").css("height", "150px");
+           $("#newsParagraph1").css("height", "100%");
            $(this).text("Less..");
            
        } else {
            
-           $(".newsParagraph").css("height", "80px");
+           $("#newsParagraph1").css("height", "80px");
            $(this).text("More..");
        }
        return false;
    });
+   $("#news2").click(function() {
+       
+       var val = $(this).text();
+       
+       if (val === "More..") {
+           
+           $("#newsParagraph2").css("height", "100%");
+           $(this).text("Less..");
+           
+       } else {
+           
+           $("#newsParagraph2").css("height", "80px");
+           $(this).text("More..");
+       }
+       return false;
+   });
+   $("#news3").click(function() {
+       
+       var val = $(this).text();
+       
+       if (val === "More..") {
+           
+           $("#newsParagraph3").css("height", "100%");
+           $(this).text("Less..");
+           
+       } else {
+           
+           $("#newsParagraph3").css("height", "80px");
+           $(this).text("More..");
+       }
+       return false;
+   });
+   $("#news4").click(function() {
+       
+       var val = $(this).text();
+       
+       if (val === "More..") {
+           
+           $("#newsParagraph4").css("height", "100%");
+           $(this).text("Less..");
+           
+       } else {
+           
+           $("#newsParagraph4").css("height", "80px");
+           $(this).text("More..");
+       }
+       return false;
+   });
+
 });
+    
+//   $(".expand").click(function() {
+       
+//       var val = $(this).text();
+       
+//       if (val === "More..") {
+           
+//           $(".newsParagraph").css("height", "100%");
+//           $(this).text("Less..");
+           
+//       } else {
+           
+//           $(".newsParagraph").css("height", "80px");
+//           $(this).text("More..");
+//       }
+//       return false;
+//   });
