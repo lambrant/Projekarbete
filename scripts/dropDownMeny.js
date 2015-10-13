ad72c2b66dd433c8967bb419a5d473ef2e7f7b81
lilienberg <lilienberg@hotmail.com>
2015-10-13 11:24:29 +0000

    Menu settings

--- scripts/dropDownMeny.js
+++ scripts/dropDownMeny.js
@@ -1,15 +1,30 @@
-var dropMenuClicker = document.getElementById("dropMenuLogo");
-var dropMenu = document.getElementById("dropMenu");
-dropMenu.style.display = "none";
-dropMenuClicker.onclick = function()
-{
-    if(dropMenu.style.display  === "none")
-    {
-        dropMenu.style.display = "block";
-        // console.log("Case1");        
-    } else
-    {
-        dropMenu.style.display = "none";
-        // console.log("Case2"); 
-    }
-}
+var isDown = false;
+$("#dropMenuLogo").click(function() {
+    if (isDown === false) {
+        $("#dropMenu").slideDown("slow")
+        isDown = true;
+    } else {
+        $("#dropMenu").slideUp("slow")
+        isDown = false;
+    } 
+})
+
+
+
+
+
+// var dropMenuClicker = document.getElementById("dropMenuLogo");
+// var dropMenu = document.getElementById("dropMenu");
+// // dropMenu.style.display = "none";
+// dropMenuClicker.onclick = function()
+// {
+//     if(dropMenu.style.display  === "none")
+//     {
+//         dropMenu.style.display = "block";
+//         // console.log("Case1");        
+//     } else
+//     {
+//         dropMenu.style.display = "none";
+//         // console.log("Case2"); 
+//     }
+// }
